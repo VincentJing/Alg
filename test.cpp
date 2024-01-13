535020f5cb528923710f9e6c8f549ecb18fed7ac
景宗飞 <jingzongfei@jingzongfeideMacBook-Pro.local>
2024-01-13 11:02:44 +0800

    Init

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int bp(string &s, unordered_set<string> &flag, int start)
+{
+    if (start == s.size())
+        return 0;
+
+    int res = 0;
+    string cur;
+
+    for (int end = start; end < s.size(); ++end)
+    {
+        cur.push_back(s[end]);
+
+        if (flag.count(cur))
+            continue;
+
+        flag.insert(cur);
+
+        res = max(res, 1 + bp(s, flag, end + 1));
+
+        flag.erase(cur);
+    }
+
+    return res;
+}
+
+int get_nums(string s)
+{
+    unordered_set<string> flag;
+
+    return bp(s, flag, 0);
+}
+
+int main()
+{
+    string s = "abacaba";
+
+    int res = get_nums(s);
+
+    cout << "Res is " << res << endl;
+}
