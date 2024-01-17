f29e9bfd22ae448f902b4d8ed0fb4bb023ade692
zongfeijing <jingzf0214@gmail.com>
2024-01-17 19:35:40 +0800

    Add leetcode 74

---
+++ Algorithm/leetcode-74.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        m = matrix.size();
+        n = matrix[0].size();
+
+        return search(matrix, target, 0, m * n - 1);
+    }
+
+    bool search(vector<vector<int>> &matrix, int target, int left, int right)
+    {
+        if (left > right)
+            return false;
+
+        int mid = (left + right) / 2;
+
+        int i = mid / n;
+        int j = mid % n;
+
+        if (target == matrix[i][j])
+            return true;
+        else if (target < matrix[i][j])
+        {
+            right = mid - 1;
+            return search(matrix, target, left, right);
+        }
+        else
+        {
+            left = mid + 1;
+            return search(matrix, target, left, right);
+        }
+
+        return true;
+    }
+
+public:
+    int m, n;
+};
